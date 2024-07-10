f1e219e17b34d818790a5526d0ae288810a1b88a
Hasini Navodya <nivodya2001@gmail.lk>
2024-07-10 17:25:44 +0530

    "Physio Full crud addedd"

--- secondtry-front/src/Physio/ViewPhysio.jsx
+++ secondtry-front/src/Physio/ViewPhysio.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 export default function ViewPhysio() {
-  const [employee, setEmployee] = useState({
+  const [physio, setPhysio] = useState({
     firstName: "",
     lastName: "",
     email: "",
@@ -12,11 +12,11 @@ export default function ViewPhysio() {
   const { id } = useParams();
 
   useEffect(() => {
-    const loadEmployee = async () => {
-      const result = await axios.get(`http://localhost:8080/api/employees/${id}`);
-      setEmployee(result.data);
+    const loadPhysio = async () => {
+      const result = await axios.get(`http://localhost:8080/api/physios/${id}`);
+      setPhysio(result.data);
     };
-    loadEmployee();
+    loadPhysio();
   }, [id]);
 
   return (
@@ -30,18 +30,18 @@ export default function ViewPhysio() {
               Details of Employee id: {id}
               <ul className="list-group list-group-flush">
                 <li className="list-group-item">
-                  <b>First Name:</b> {employee.firstName}
+                  <b>First Name:</b> {physio.firstName}
                 </li>
                 <li className="list-group-item">
-                  <b>Last Name:</b> {employee.lastName}
+                  <b>Last Name:</b> {physio.lastName}
                 </li>
                 <li className="list-group-item">
-                  <b>Email:</b> {employee.email}
+                  <b>Email:</b> {physio.email}
                 </li>
               </ul>
             </div>
           </div>
-          <Link className="btn btn-primary my-2" to={"/home"}>
+          <Link className="btn btn-primary my-2" to={"/physiohome"}>
             Back to Home
           </Link>
         </div>
--- secondtry-front/src/pages/NewHome.jsx
+++ secondtry-front/src/pages/NewHome.jsx
@@ -176,17 +176,17 @@ const NewHome = () => {
           <h2>LOCATE US</h2>
         </div>
         <div className="google-map">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.982584570147!2d79.92213817448265!3d6.892686118764102!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae25138514423cb%3A0x981f4120a1d021d1!2sTHEPHYSIO%20Clinic!5e0!3m2!1sen!2slk!4v1698731444163!5m2!1sen!2slk"
-            width="600"
-            height="450"
-            style={{ border: 0 }}
-            allowFullScreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-            title="Google Maps"
-          ></iframe>
-        </div>
+        <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.9451852844486!2d79.8726168!3d6.897159900000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae259ee2a2b0533%3A0xe2776ef7575a7845!2sThe%20Physio!5e0!3m2!1sen!2slk!4v1720498977765!5m2!1sen!2slk"
+        width="600"
+        height="450"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+        title="Google Maps"
+      ></iframe>
+    </div>
       </div>
 
       {/* APPOINTMENT section */}
