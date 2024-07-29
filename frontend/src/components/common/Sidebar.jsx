62ac052914811023905cdb524689f19d6e83c3d2
Hasini Navodya <nivodya2001@gmail.lk>
2024-07-29 18:48:22 +0530

    Landingpage changed

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -63,9 +63,9 @@ function Sidebar() {
                     <a href='/dashboard' className={`flex-none ${isCollapsed ? 'hidden' : ''}`}>
                         <img src="./src/assets/logowithoutback.png" width={isCollapsed ? 50 : 140} className="mx-auto mt-12" />
                     </a>
-                    <button onClick={() => setIsCollapsed(!isCollapsed)} className="text-white focus:outline-none">
+                    {/* <button onClick={() => setIsCollapsed(!isCollapsed)} className="text-white focus:outline-none">
                         <FaBars size={24} color="#ffffff" />
-                    </button>
+                    </button> */}
                 </div>
                 <div className="flex-1 flex flex-col h-full overflow-auto mt-12">
                     <ul className="px-4 text-sm font-medium flex-1">
