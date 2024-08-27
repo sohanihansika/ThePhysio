1909ef1e8c531644adf3a2e27898ece575573309
sohanihansika <136710534+sohanihansika@users.noreply.github.com>
2024-08-27 13:16:27 +0530

    remove and fix backend errors

--- ThePhysio/src/main/java/GroupProjectII/ThePhysio/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package GroupProjectII.ThePhysio.repository;
-
-import GroupProjectII.ThePhysio.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
