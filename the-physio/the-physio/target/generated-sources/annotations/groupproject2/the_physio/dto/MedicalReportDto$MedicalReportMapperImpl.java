60e2e7599118591eb43128e4a75f3e27ca63ac86
Isuri Wagachchi <isuriwagachchi19@gmail.com>
2024-11-29 17:53:59 +0530

    new commit

--- the-physio/the-physio/target/generated-sources/annotations/groupproject2/the_physio/dto/MedicalReportDto$MedicalReportMapperImpl.java
+++ the-physio/the-physio/target/generated-sources/annotations/groupproject2/the_physio/dto/MedicalReportDto$MedicalReportMapperImpl.java
@@ -7,7 +7,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-11-06T13:24:21+0530",
+    date = "2024-11-29T15:22:41+0530",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 22.0.1 (Oracle Corporation)"
 )
 public class MedicalReportDto$MedicalReportMapperImpl implements MedicalReportDto.MedicalReportMapper {
